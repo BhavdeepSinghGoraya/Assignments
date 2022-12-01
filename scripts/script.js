5779d4afafa285a73d231b7b70b5fc8e7dc93804
Bhavdeep Singh <gorayabhavdeep@gmail.com>
2022-11-30 17:36:59 -0800

    Adding dark them in the page using toggle in javascript

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const clickDarkTheme = document.querySelector('.DarkTheme');
+const SideBar = document.querySelector('aside');
+const cancelBtn = document.querySelector('.Cancel');
+const saveBtn = document.querySelector('.Save');
+const textArea = document.querySelector('textarea');
+const newNoteBtn = document.querySelector('.Note');
+const notesArray =[ {title:"note one" , body:"this is my first note"},
+                    {title:"note two" , body:"this is my second note"}
+                ];
+const unorderedList = document.querySelector('.notes-list'); 
+function buttonDarkTheme(e){
+    document.body.classList.toggle('dark-background-color');
+    document.body.classList.toggle('light-text-color');
+    SideBar.classList.toggle('dark-aside-color');
+    newNoteBtn.classList.toggle('btn-dark-background-color');
+    clickDarkTheme.classList.toggle('darktheme-background-color');
+    cancelBtn.classList.toggle('cancel-background-color');
+    saveBtn.classList.toggle('btn-dark-background-color');
+};
+
+clickDarkTheme.addEventListener('click', buttonDarkTheme);
+
+function changeText(e){
+    if ( clickDarkTheme.textContent === "Dark Theme"){
+        clickDarkTheme.textContent = "Light Theme"
+    }
+    else{
+        clickDarkTheme.textContent = "Dark Theme"
+    }
+};
+
+clickDarkTheme.addEventListener('click', changeText);
